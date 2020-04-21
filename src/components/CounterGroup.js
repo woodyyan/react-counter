fe24572c3c1e91accdd0e18f4ff9ad447d5eb46a
Songbai Yan <colorguitar@hotmail.com>
2020-04-21 16:13:42 +0800

    add onchange event

--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -6,17 +6,31 @@ export default class CounterGroup extends Component {
         super(props)
 
         this.initArray = this.initArray.bind(this)
+        this.onChange = this.onChange.bind(this)
+
+        this.state = {
+            size: 2
+        }
     }
 
+    onChange(event) {
+        const value = event.target.value
+        this.setState({
+            size: value.length > 0 ? parseInt(value) : 0
+        })
+    }
 
     initArray(size) {
         return Array.from(Array(size).keys())
     }
 
     render() {
-        let counters = this.initArray(100)
+        let counters = this.initArray(this.state.size)
         return (
             <div>
+                <form>
+                    <input type="text" onChange={this.onChange} value={this.state.size}/>
+                </form>
                 {
                     counters.map(value => (
                         <Counter key={value}/>
