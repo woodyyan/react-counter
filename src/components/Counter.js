26dc405aef86cd12ae457c6fcc00ecc9699e334d
Songbai Yan <colorguitar@hotmail.com>
2020-04-21 14:09:18 +0800

    add on increase

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,15 +4,22 @@ class Counter extends Component {
     constructor(props) {
         super(props)
 
+        this.onIncrease = this.onIncrease.bind(this)
         this.state = {
             number: 0
         }
     }
 
+    onIncrease() {
+        this.setState((preState) => ({
+            number: preState.number + 1
+        }))
+    }
+
     render() {
         return (
             <div>
-                <button>+</button>
+                <button onClick={this.onIncrease}>+</button>
                 <span>{this.state.number}</span>
                 <button>-</button>
             </div>
